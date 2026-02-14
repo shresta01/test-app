df74d1b93e46b15a986fd3a72e210b0fee5e4088
Shresta U <97824566+shresta01@users.noreply.github.com>
2026-02-14 11:32:22 +0530

    Wrap Routes in Router for routing functionality

--- val/valentine-app/src/App.js
+++ val/valentine-app/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import "./App.css";
 import YayPage from "./YayPage";
 
@@ -45,43 +45,45 @@ function App() {
   };
 
   return (
-    <Routes>
-      <Route
-        path="/"
-        element={
-          <div className="App">
-            <h1 className="question">💖 Will you be my Valentine?? 💖</h1>
-            
-            {/* Word Grid */}
-            <div className="word-container">
-              {word.split("").map((char, i) => (
-                <div key={i} className="letter-grid">
-                  {letterPatterns[char]?.map((row, r) => (
-                    <div key={r} className="row">
-                      {row.map((cell, ci) => (
-                        <span key={ci} className="cell">
-                          {cell === "YES" ? (
-                            <Link to="/yay" className="yes-block">YES</Link>
-                          ) : (
-                            <span className="empty"></span>
-                          )}
-                        </span>
-                      ))}
-                    </div>
-                  ))}
-                </div>
-              ))}
-            </div>
+    <Router basename={process.env.PUBLIC_URL}>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <div className="App">
+              <h1 className="question">💖 Will you be my Valentine?? 💖</h1>
+              
+              {/* Word Grid */}
+              <div className="word-container">
+                {word.split("").map((char, i) => (
+                  <div key={i} className="letter-grid">
+                    {letterPatterns[char]?.map((row, r) => (
+                      <div key={r} className="row">
+                        {row.map((cell, ci) => (
+                          <span key={ci} className="cell">
+                            {cell === "YES" ? (
+                              <Link to="/yay" className="yes-block">YES</Link>
+                            ) : (
+                              <span className="empty"></span>
+                            )}
+                          </span>
+                        ))}
+                      </div>
+                    ))}
+                  </div>
+                ))}
+              </div>
 
-            {/* Extra Button */}
-            <Link to="/yay">
-              <button className="valentine-button">OFCOURSE YES!</button>
-            </Link>
-          </div>
-        }
-      />
-      <Route path="/yay" element={<YayPage />} />
-    </Routes>
+              {/* Extra Button */}
+              <Link to="/yay">
+                <button className="valentine-button">OFCOURSE YES!</button>
+              </Link>
+            </div>
+          }
+        />
+        <Route path="/yay" element={<YayPage />} />
+      </Routes>
+    </Router>
   );
 }
 
